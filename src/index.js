81f49e98b05eda72ea1308d533257d51b547a93f
Ryan Pfister <interpfister@gmail.com>
2017-06-05 08:58:18 -0400

    Update intent schema, utterances and skill title

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 var languageStrings = {    
     "en-US": {
         "translation": {            
-            "SKILL_NAME" : "Everytown Gun Facts (unofficial)",
+            "SKILL_NAME" : "Gun Safety Facts",
             "HELP_MESSAGE" : "Ask me for a fact about gun safety!",
             "HELP_REPROMPT" : "Say: give me a fact",
             "STOP_MESSAGE" : "Goodbye!"
